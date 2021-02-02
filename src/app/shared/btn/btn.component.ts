0394f613e24ef767d57fcf72cadbf4f6ec2ce16d
moaz_home <moazHome@gmail.com>
2021-02-02 20:50:22 +0200

    create btn component v_1

---
+++ src/app/shared/btn/btn.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: "app-btn",
+  templateUrl: "./btn.component.html",
+  styleUrls: ["./btn.component.scss"],
+})
+export class BtnComponent implements OnInit {
+  
+  // init props & inputs
+  @Input("btn_title") btn_title: string;
+  @Input("color") color: string;
+  @Input("btn_type") btn_type: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InputFiledComponent } from './input-filed/input-filed.component';
+import { BtnComponent } from './btn/btn.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, InputFiledComponent],
+  declarations: [NavbarComponent, InputFiledComponent, BtnComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent, InputFiledComponent],
+  exports: [NavbarComponent, InputFiledComponent, BtnComponent],
 })
 export class SharedModule {}
