a4e99034c70f34cc0f1dd5a1ce2ae5e9bedc46ca
moaz_home <moazHome@gmail.com>
2021-02-07 04:59:38 +0200

    add api delete card

--- src/app/edit-profile/components/payment/payment.component.ts
+++ src/app/edit-profile/components/payment/payment.component.ts
@@ -1,3 +1,5 @@
+import { ProfileService } from './../../../services/profile.service';
+import { EditProfileService } from 'src/app/services/edit-profile.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,7 +13,7 @@ export class PaymentComponent implements OnInit {
   // init name & type vars
   cards: {}[] | any
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private EditProfile:EditProfileService, private Profile:  ProfileService) {
 
     // init vars
     this.route.data.subscribe((res) => this.cards = res.cards.data);
@@ -20,8 +22,12 @@ export class PaymentComponent implements OnInit {
 
   ngOnInit() {}
 
+  // func deleteCard run => delete card from profile user
   deleteCard(card_id: string | number) {
-
+    this.EditProfile.deleteCard({card_id}).subscribe((res: any) => {
+      //after delete card rom profile user => get list of cards user
+      res.success ?  this.Profile.getCardsUser().subscribe((res: any) => this.cards = res.data) : null
+    })
   }
 
 }
--- src/app/services/edit-profile.service.ts
+++ src/app/services/edit-profile.service.ts
@@ -7,14 +7,17 @@ import { Injectable } from '@angular/core';
 export class EditProfileService {
   constructor(private http: HttpClient) {}
 
+  //post PersonalDetails
   editPersonalDetails(data: {}) {
     return this.http.post("auth/edit-profile", data);
   }
 
+  //post PofileSetting
   editPofileSetting(data: {}) {
     return this.http.post("auth/setting", data);
   }
 
+  // start post api Address
   addAddress(data: {}) {
     return this.http.post("auth/address-book/add", data);
   }
@@ -30,4 +33,11 @@ export class EditProfileService {
   deleteAddress(addressID: {}) {
     return this.http.post("auth/address-book/delete", addressID);
   }
+  // end post api Address
+
+  //start post api cards
+  deleteCard(cardID: {}) {
+    return this.http.post("auth/cards/delete", cardID);
+  }
+  //end post api cards
 }
